1d55006574fb8357899ca83b2153893f698b65cc
Lei-Jue <leijue1999@gmail.com>
2022-11-06 21:37:10 +0800

    second commit

---
+++ git-command.R
@@ -0,0 +1,30 @@
+
+# some of command in git version, change in "2022-11-06 21:33:15 CST"
+git config --global user.name "Lei-Jue"
+git config --global user.email "leijue1999@gmail.com"
+git config --global --list
+
+#3) Push an existing repository
+#from the RStudio menu, follow:
+#  Tools -> Terminal -> New Terminal
+#enter the following lines in the Terminal
+#with your username and repository name
+git remote add origin https://github.com/Lei-Jue/AFEC-2022.git
+# if false, try to replace origin to dev. from https://komodor.com/learn/how-to-fix-fatal-remote-origin-already-exists-error/
+git remote add dev https://github.com/Lei-Jue/AFEC-2022.git
+git add .# add all file in folder to commit 
+git commit -m "first commit"
+git push -u dev master
+
+git remote -v
+#tit@mi MINGW64 /e/data/git (master)
+#$ git remote -v
+#dev     https://github.com/Lei-Jue/AFEC-2022.git (fetch)
+#dev     https://github.com/Lei-Jue/AFEC-2022.git (push)
+#origin  https://github.com/Lei-Jue/AFEC_X_2022.git (fetch)
+#origin  https://github.com/Lei-Jue/AFEC_X_2022.git (push)
+
+
+
+
+
